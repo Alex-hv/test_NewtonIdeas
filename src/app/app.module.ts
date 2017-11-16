38dffc6f40083ed52f6640a5bbe5c368b686370e
Alexandr Goncharov <goncharov.a.v.97@gmail.com>
2017-11-16 23:46:43 +0200

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
+import { NewsListComponent } from './news-list/news-list.component';
+import {RouterModule} from "@angular/router";
+import { AddPostComponent } from './add-post/add-post.component';
+import {HttpModule} from "@angular/http";
+import {DataService} from "./shared/data.service";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+const routes = [
+  {path: '', redirectTo: 'news', pathMatch: 'full'},
+  {path: 'news', component: NewsListComponent},
+  {path: 'add', component: AddPostComponent},
+  {path: 'details/:id', component: DetailsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent,
+    NewsListComponent,
+    AddPostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
